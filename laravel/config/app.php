66aa3af785e0d8a47e9125eb4bf3f7696bd568aa
amekuhideki <h_ameku@funteam.co.jp>
2018-04-27 01:46:21 +0900

    Add library laravelcollective

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -178,6 +178,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Add laravelcollective
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -226,6 +229,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        //Add laravelcollective
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
